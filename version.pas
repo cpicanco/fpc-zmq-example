bc38d2fdc0068ffdbc0cb727caedbe886bace06d
cpicanco <cpicanco42@gmail.com>
2017-08-02 22:29:52 -0300

    add version example

---
+++ version.pas
@@ -0,0 +1,12 @@
+{
+  Report 0MQ version
+  @author cpicanco <cpicanco@ufpa.br>
+}
+program version;
+
+uses zmq.version;
+
+begin
+  ZMQVersion;
+end.
+
---
+++ zmq.version.pas
@@ -0,0 +1,29 @@
+{
+  fpc-zmq
+  Copyright (C) 2017 Carlos Rafael Fernandes Picanço.
+
+  The present file is distributed under the terms of the GNU Lesser General Public License (LGPL v3.0).
+
+  You should have received a copy of the GNU Lesser General Public License
+  along with this program. If not, see <http://www.gnu.org/licenses/>.
+}
+unit zmq.version;
+
+interface
+
+procedure ZMQVersion;
+
+implementation
+
+uses zmq;
+
+procedure ZMQVersion;
+var
+  major, minor, patch : integer;
+begin
+  zmq_version(major, minor, patch);
+  WriteLn(major,'.',minor,'.',patch);
+end;
+
+end.
+
